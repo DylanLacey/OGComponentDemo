deb55b7f1d3e9855a2fb5e9198629f9ebbd1d803
The Gentlehacker <github@dylanlacey.com>
2024-01-29 13:25:52 +1000

    Made OG Metadata link absolute to avoid webembed tears

--- src/routes/metadataExamples/website/+page.svelte
+++ src/routes/metadataExamples/website/+page.svelte
@@ -1,18 +1,23 @@
 <script>
     export let data
+    import { dev } from "$app/environment";
 
     // Photo by Valdemar Kostenko on Unsplash: https://unsplash.com/photos/brown-tabby-cat-on-brown-wooden-table-aqBTmo6oUA8
     import imageUrl from "$lib/images/valdemar-kostenko-aqBTmo6oUA8-unsplash-og-resize.jpg"
+
+    let absoluteImageUrl = dev ? imageUrl : data.ownHostPrefix + imageUrl
 </script>
 
 <svelte:head>
     <meta property="og:title" content="My Website" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content={data.ownUrl}/>
-    <meta property="og:image" content={imageUrl}/>
+    <meta property="og:image" content={absoluteImageUrl}/>
     <meta property="og:description" content="An example of a webpage, for the purposes of illustrating how Open Graph metadata may be displayed." />
 </svelte:head>
 
 <div>
     <h1>Metadata!</h1>
+    <h2>For Websites!</h2>
+
 </div>
--- src/routes/metadataExamples/website/+page.ts
+++ src/routes/metadataExamples/website/+page.ts
@@ -3,6 +3,7 @@ import { dev } from "$app/environment";
 
 export const load: PageLoad = ({ url }) => {
     return {
-        ownUrl: dev ? `${url.pathname}` : url.toString()
+        ownUrl: dev ? `${url.pathname}` : url.toString(),
+        ownHostPrefix: dev ? `/` : `${url.origin}`
     }
 }
